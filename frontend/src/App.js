00b2da2d1aaac62faaf921fae10d264d91c4c244
Arti Yadav <arti2812002@gmail.com>
2023-03-07 11:34:07 +0530

    adding navbar and changing file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,49 @@
 import './App.css';
-import Summary from './components/Display/Summary';
-import PC from './components/Display/PC';
-import PeerComp from './components/Display/PeerComp';
-import QuaterlyResults from './components/Display/QuaterlyResults';
-import PF from './components/Display/PF';
-import BalanceSheet from './components/Display/BalanceSheet';
-import CashFlows from './components/Display/CashFlows';
-import Ratios from './components/Display/Ratios';
-import Document from './components/Display/Document';
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Container, CssBaseline } from "@mui/material";
+import { ToastContainer } from "react-toastify";
+import Box from "@mui/material/Box";
+import CompanyInfo from './pages/CompanyInfo';
+import SearchAppBar from './components/UI/Navbar';
+import "react-toastify/dist/ReactToastify.css";
 
 
 function App() {
 
   
   return (
-    
-    <div className="App">
-      <Summary />
-      <PC />
-      <PeerComp />
-      <QuaterlyResults />
-      <PF />
-      <BalanceSheet />
-      <CashFlows />
-      <Ratios />
-      <Document />
+    <BrowserRouter>
+      <CssBaseline />
+      <Box sx={{ position: "relative", minHeight: "100vh" }}>
+        <SearchAppBar />
+        <Box sx={{ paddingTop: "5em", paddingBottom: "4rem" }}>
+        <Container maxWidth={false} sx={{ maxWidth: '85%' }}>
+          <CompanyInfo/>
+          <Routes>
+            {/* <Route exact path="/" element={<LandingPage/>} />
+            <Route exact path="/login" element={<Login />}></Route>
+            <Route exact path="/register" element={<Register />}></Route> */}
+            {/* <Route
+              path="/"
+              element={
+                <ProtectedRoutes>
+                  <SharedLayout />
+                </ProtectedRoutes>
+              }
+            > */}
+              {/* OTHER PROTECTED ROUTES */}
+              {/* <Route path="/home" element={<Home/>} />
+              <Route path="/dashboard" element={<Dashboard />} /> */}
+            {/* </Route> */}
 
-
-    </div >
+            {/* ERROR PAGE */}
+            {/* <Route exact path="*" element={<Error />}></Route> */}
+          </Routes>
+          <ToastContainer position="top-center" />
+          </Container>
+        </Box>
+      </Box>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/CompanyInfo.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Summary from '../components/Display/Summary'
+import PC from '../components/Display/PC'
+import PeerComp from '../components/Display/PeerComp'
+import QuaterlyResults from '../components/Display/QuaterlyResults'
+import PF from '../components/Display/PF'
+import BalanceSheet from '../components/Display/BalanceSheet'
+import CashFlows from '../components/Display/CashFlows'
+import Ratios from '../components/Display/Ratios'
+import Document from '../components/Display/Document'
+function CompanyInfo(props) {
+  return (
+    <>
+        <Summary />
+        <PC />
+        <PeerComp />
+        <QuaterlyResults />
+        <PF />
+        <BalanceSheet />
+        <CashFlows />
+        <Ratios />
+        <Document />
+    </>
+  )
+}
+
+CompanyInfo.propTypes = {}
+
+export default CompanyInfo

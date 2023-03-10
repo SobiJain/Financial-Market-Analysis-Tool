b784fdfed3b2c490bcc578d8b1559f33ffe959b6
Devika-Sahu <75083536+Devika-Sahu@users.noreply.github.com>
2023-03-10 16:21:25 +0530

    added files to retrieve data from api

---
+++ market_analyser/api/ratios.py
@@ -0,0 +1,38 @@
+
+import requests
+import json
+
+url_balanceSheet = "https://yh-finance.p.rapidapi.com/stock/v2/get-balance-sheet"
+url_cashflow = "https://yh-finance.p.rapidapi.com/stock/v2/get-cash-flow"
+querystring = {"symbol":"aapl","region":"US"}
+
+headers = {
+	"X-RapidAPI-Key": "2c1bb36bc9msh1d26faa4230b73ap148512jsna77f5c4762cd",
+	"X-RapidAPI-Host": "yh-finance.p.rapidapi.com"
+}
+
+response_balanceSheet = requests.request("GET", url_balanceSheet, headers=headers, params=querystring)
+response_cashflow = requests.request("GET", url_cashflow, headers=headers, params=querystring)
+
+data_balanceSheet = json.loads(response_balanceSheet.content)
+data_cashflow = json.loads(response_cashflow.content)
+
+
+balance_sheet_data = data_balanceSheet["balanceSheetHistory"]['balanceSheetStatements']
+cashflow_data = data_cashflow['cashflowStatementHistory']['cashflowStatements']
+
+
+roeList = [[] for _ in range(len(cashflow_data)+1)]
+roeList[0].append('endDate')
+roeList[0].append('ROE %')
+print(roeList)
+
+index = 1
+
+for i in balance_sheet_data:
+    for j in cashflow_data:
+        if i['endDate']['fmt']==j['endDate']['fmt']:
+            roeList[index].append(i['endDate']['fmt'])
+            roeList[index].append((float(i['totalStockholderEquity']['raw'])/float(j['netIncome']['raw']))*100)
+    index+=1
+print(roeList)

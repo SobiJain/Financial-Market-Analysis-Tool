d510277bb4cc6015b1616082d02946f02d0127b4
SobiJain <sobi3jain1@gmail.com>
2023-03-13 21:52:45 +0530

    added profile section

--- market_analyser/api/urls.py
+++ market_analyser/api/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes, name="routes")
+    path('', views.getRoutes, name="routes"),
+    path('cashflow', views.getCashflow, name="cashflow"),
+    path('balancesheet', views.getBalanceSheet, name="balancesheet"),
+    path('ratio', views.getRatio, name="ratio"),
+    path('profile', views.getProfile, name="profile"),
 ]
--- market_analyser/api/views.py
+++ market_analyser/api/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-# Create your views here.
+import requests
+import json
+from . import cashflow, balancesheet, ratios, profile
 
+# Create your views here.
 def getRoutes(request):
-    return JsonResponse('Our Api', safe=False)
+    return JsonResponse("henlo", safe=False)
+
+def getCashflow(request):
+    return JsonResponse(cashflow.cashflowList, safe=False)
+
+def getBalanceSheet(request):
+    return JsonResponse(balancesheet.balanceSheetList, safe=False)
+
+def getRatio(request):
+    return JsonResponse(ratios.roeList, safe=False)
+
+def getProfile(request):
+    return JsonResponse(profile.data_profile, safe=False)

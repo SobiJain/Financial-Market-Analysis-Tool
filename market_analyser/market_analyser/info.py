e857f9d471f0f6c3d61db8bb6be8141a6d8f0de4
Devika-Sahu <75083536+Devika-Sahu@users.noreply.github.com>
2023-03-23 14:33:04 +0530

    Adding backend for user auth

---
+++ market_analyser/market_analyser/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'stockscreener.deshaw'
+EMAIL_HOST_PASSWORD = 'itlpjlyzfydwbscf'
+EMAIL_PORT = 587
--- market_analyser/market_analyser/settings.py
+++ market_analyser/market_analyser/settings.py
@@ -11,9 +11,17 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .info import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+EMAIL_USE_TLS = EMAIL_USE_TLS
+EMAIL_HOST = EMAIL_HOST
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+EMAIL_PORT = EMAIL_PORT
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +65,7 @@ ROOT_URLCONF = "market_analyser.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -118,6 +126,8 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 

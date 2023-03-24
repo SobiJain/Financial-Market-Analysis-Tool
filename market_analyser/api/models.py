338afb5a4e6fe7fd88311f5023ec61ecce3d70b3
Devika-Sahu <75083536+Devika-Sahu@users.noreply.github.com>
2023-03-24 23:23:16 +0530

    addin auth

--- market_analyser/api/models.py
+++ market_analyser/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+class User(models.Model):
+
+    email = models.EmailField(max_length=100, unique=True)
+    otp = models.CharField(max_length=6)
+    verified = models.BooleanField()
+    password = models.CharField(max_length=50,unique=True)
+    
+    def __str__(self):
+        return self.email
--- market_analyser/api/urls.py
+++ market_analyser/api/urls.py
@@ -10,5 +10,11 @@ urlpatterns = [
     path('quarter', views.getQuarter, name="quarter"),
     path('profitloss', views.getProfitLoss, name="profitloss"),
     path('companies',views.getCompanyList,name='companies'),
-    path('price', views.getPrice, name='price')
+    path('price', views.getPrice, name='price'),
+
+    path('register', views.register, name='register'),
+    path('verify', views.verify, name='verify'),
+    path('login', views.login, name='login'),
+    path('forgot', views.forgot, name='forgot'),
+    path('reset', views.reset, name='reset'),
 ]

37b974b88a14faeec005cd14ff0fd141aadff29a
Devika-Sahu <75083536+Devika-Sahu@users.noreply.github.com>
2023-04-07 00:55:18 +0530

    changing email and password

--- market_analyser/market_analyser/settings.py
+++ market_analyser/market_analyser/settings.py
@@ -97,8 +97,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'marketanalyserproj@gmail.com'
-EMAIL_HOST_PASSWORD = 'rabznbrjggeojvbl'
+EMAIL_HOST_USER = 'stockscreener.deshaw@gmail.com'
+EMAIL_HOST_PASSWORD = 'itlpjlyzfydwbscf'
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
